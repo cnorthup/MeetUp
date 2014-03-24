a08a06c7c938f12a96ab8d45561b3ad37d948931
Charles Northup <cjnorth40@gmail.com>
2014-03-24 13:15:44 -0500

    Initial Commit

---
+++ MeetUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeetUp
+//
+//  Created by Charles Northup on 3/24/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
